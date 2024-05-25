7b512b09c67cb93f2a888a95e1d24242b9b13032
RehanaMudassar <rehana.mudassar78@gmail.com>
2024-05-25 14:08:42 +0500

    Program is completely error free

---
+++ index.ts
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+// console.log("Currency _Converter")
+const currency: any ={
+    USD: 1   ,   //IT WILL BE THE BASE CURRENCY FUSED FOR CONVERSION IN THIS PROJECT
+    EUR:0.91,
+    INR: 74.57,
+    PKR: 280,
+    QTR: 0.27
+};
+let user_answer= await inquirer .prompt(
+    [
+        {
+            name: "from",
+            type:  "list",
+            choices:["USD","EUR","PKR", "QTR"],
+            message: "Enter the currency to be converted"
+        },
+        {
+            name: "to",
+            type:  "list",
+            choices:["USD","EUR","PKR", "QTR"],
+            message: "Enter the currency in which  conversion is required"
+        },
+        {name: "amount",
+        message: "converted amount is",
+        type: "number"
+
+
+        }
+    ]
+)
+ let fromAmount=currency[user_answer.from]
+ let toAmount=currency[user_answer.to]
+let amount=user_answer.amount
+console.log(fromAmount)
+console.log(toAmount)
+console.log(amount)
